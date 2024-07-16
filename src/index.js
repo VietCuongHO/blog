9c1ca7006e56c94b195c0a9f524c2eef8ac50854
cho47 <cuongviet.ho@dxc.com>
2024-07-16 11:01:13 +0700

    template engine

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-const express = require('express')
-const morgan = require('morgan')
+import express from 'express'
+import morgan from 'morgan'
+import {engine} from 'express-handlebars'
+import path from 'path'
+
 const app = express()
 const port = 3000
+const __dirname = path.resolve()
 
+//Template engine 
+app.engine('hbs', engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'src/resources/views'))
+
+//Http logger: Morgan will help to log http request 
 app.use(morgan('combined'))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home')
+})
+
+app.get('/news', (req, res) => {
+  res.render('news')
 })
 
 app.listen(port, () => {
